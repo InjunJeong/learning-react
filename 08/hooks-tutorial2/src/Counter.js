663bd38a13e7e79d502a9447b730ee87525629dd
정인준 <jeonginun87@gmail.com>
2023-03-15 09:17:52 +0000

    Update 22 files

---
+++ 08/hooks-tutorial2/src/Counter.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+
+const Counter = () => {
+    const[value, setValue] = useState(0);
+    
+    return (
+        <div>
+            <p>
+                현재 카운터 값은 <b>{value}</b>입니다.
+            </p>
+            <butto onClick={() => setValue(value + 1)}>+ 1</butto>
+            <butto onClick={() => setValue(value - 1)}>- 1</butto>
+        </div>
+    );
+}
+export default Counter;
